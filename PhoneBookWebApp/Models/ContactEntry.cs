6e7e1a8d6e7cbf45e4d0969a45ccc11070586e10
Stella Tryfona <stellatryfona@gmail.com>
2020-03-21 09:26:55 +0200

    Add ContactEntry and ContactPhoneEntry DTOs for creating new contact or new phone for existing contact

---
+++ PhoneBookWebApp/Models/ContactEntry.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhoneBookWebApp.Models
+{
+    public class ContactEntry
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public ICollection<ContactPhoneEntry> ContactPhones { get; set; }
+    }
+}
