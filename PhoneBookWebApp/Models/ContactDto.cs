27f6121b65f6b0f7a1d07c2e0b6467c116ac7555
Stella Tryfona <stellatryfona@gmail.com>
2020-03-21 09:17:38 +0200

    Add DTOs for Contact and ContacPhone

---
+++ PhoneBookWebApp/Models/ContactDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhoneBookWebApp.Models
+{
+    public class ContactDto
+    {
+        public int ContactId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public ICollection<ContactPhoneDto> ContactPhones { get; set; }
+    }
+}
---
+++ PhoneBookWebApp/Models/ContactPhoneDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PhoneBookWebApp.Models
+{
+    public class ContactPhoneDto
+    {
+        public int ContactPhoneId { get; set; }
+        public int ContactId { get; set; }
+        public string CountryCode { get; set; }
+        public string AreaCode { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
